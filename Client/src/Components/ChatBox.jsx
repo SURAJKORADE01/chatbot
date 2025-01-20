befbb8242e974dbef8cb76e333386f83a766be75
Suraj Korade <125278152+SURAJKORADE01@users.noreply.github.com>
2025-01-20 20:11:52 +0530

    Add files via upload

---
+++ Client/src/Components/ChatBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ChatBox({ messages }) {
+  return (
+    <div className="chat-box mb-4" style={{ maxHeight: '400px', overflowY: 'scroll' }}>
+      {messages.map((msg, index) => (
+        <div key={index} className="message mb-2">
+          <div className="alert alert-primary">
+            <span>{msg.text}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ChatBox;
