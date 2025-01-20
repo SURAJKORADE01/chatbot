6905003724dc69ed6df8284c98783ed44e231c7f
Suraj Korade <125278152+SURAJKORADE01@users.noreply.github.com>
2025-01-20 20:10:45 +0530

    Add files via upload

---
+++ Server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5000;
+
+const db = new sqlite3.Database('./chatbot.db');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/messages', (req, res) => {
+  db.all('SELECT * FROM messages', (err, rows) => {
+    if (err) {
+      res.status(500).send('Error fetching messages');
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.post('/messages', (req, res) => {
+  const { message } = req.body;
+  db.run('INSERT INTO messages (text) VALUES (?)', [message], function (err) {
+    if (err) {
+      res.status(500).send('Error inserting message');
+    } else {
+      res.json({ id: this.lastID, text: message });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
